01b34a70a6fd267a4d0fce6141d06afec82d6b0c
kahlil747 <kahlilkmassicot@gmail.com>
2020-02-06 19:47:35 +0000

    kahlil change this

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -3,15 +3,15 @@ import React from "react";
 //here we created the component Modal as a function
 function MyCard() {
 	return (
-		<div class="card" style="width: 18rem;">
-			<img class="card-img-top" src="..." alt="Card image cap" />
-			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
-				<p class="card-text">
+		<div className="card" style="width: 18rem;">
+			<img className="card-img-top" src="..." alt="Card image cap" />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">
 					Some quick example text to build on the card title and make
-					up the bulk of the card's content.
+					up the bulk of the cards content.
 				</p>
-				<a href="#" class="btn btn-primary">
+				<a href="#" className="btn btn-primary">
 					Go somewhere
 				</a>
 			</div>
